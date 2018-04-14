02e94a2764e8ce95e0abb4960d7ee5e6e4abdb8b
d o <d o>
2018-04-14 12:01:58 +0800

    ido add start navi methed

--- app/src/main/java/com/example/fei/edmapdemotest/Navigate.java
+++ app/src/main/java/com/example/fei/edmapdemotest/Navigate.java
@@ -4,7 +4,10 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.util.Log;
 
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 /**
@@ -13,6 +16,22 @@ import java.util.ArrayList;
  */
 
 public class Navigate {
+
+    //--------------以下都是高德坐标系的坐标-------------------//
+    private static final double LATITUDE_A = 34.8171930000;  //起点纬度
+    private static final double LONGTITUDE_A = 113.5372840000;  //起点经度
+
+    private static final double LATITUDE_B = 34.8171930000;  //终点纬度
+    private static final double LONGTITUDE_B = 113.5372840000;  //终点经度
+
+
+
+    //----------------以下都是百度坐标系的坐标------------------//
+    private static final double LATITUDE_QIDIAN = 34.8234278343;  //起点纬度
+    private static final double LONGTITUDE_QIDIAN = 113.5437323900;  //起点经度
+
+    private static final double LATITUDE_ZHONGDIAN = 34.8234278343;  //终点纬度
+    private static final double LONGTITUDE_ZHONGDIAN = 113.5437323900;  //终点经度
 
     public static void startNavigate(){
         setMapAppListAndListener();
@@ -32,5 +51,29 @@ public class Navigate {
     private static CharSequence[] setMapAppListAndListener(){
         //TODO 设置已安装地图软件列表和监听器
         return new CharSequence[2];
+    }
+
+    /**
+     * 我的位置BY高德
+     */
+    void setUpGaodeAppByMine(Context context){
+        try {
+            Intent intent = Intent.getIntent("androidamap://route?sourceApplication=softname&sname=我的位置&dlat="+LATITUDE_B+"&dlon="+LONGTITUDE_B+"&dname="+"钟楼广场"+"&dev=0&m=0&t=1");
+            context.startActivity(intent);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 我的位置到终点通过百度地图
+     */
+    void setUpBaiduAPPByMine(Context context){
+        try {
+            Intent intent = Intent.getIntent("intent://map/direction?origin=我的位置&destination=钟楼广场&mode=driving&src=yourCompanyName|yourAppName#Intent;scheme=bdapp;package=com.baidu.BaiduMap;end");
+            context.startActivity(intent);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 }
